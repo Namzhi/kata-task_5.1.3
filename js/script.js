577e8c038e0ac1611c86aaa3b664df301799b947
Margarita Naidanova <chimit9130@gmail.com>
2024-04-11 03:48:31 +0700

    layout 3 start

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+new Swiper('.logo-slider', {
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    
+});
