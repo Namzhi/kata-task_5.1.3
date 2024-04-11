c3235ff029401a94675754c321f6edf0109a2c7c
Margarita Naidanova <chimit9130@gmail.com>
2024-04-11 08:42:11 +0700

    buttons for 768

--- js/script.js
+++ js/script.js
@@ -13,3 +13,27 @@ new Swiper('.logo-slider', {
     spaceBetween: 16,
     
 });
+
+let buttonShow = document.querySelector(".show-more");
+let wrapper = document.querySelector('.logo-slider__wrapper');
+let buttonShowText = buttonShow.querySelector(".show-more__text");
+show();
+function show() {
+    buttonShow.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    wrapper.classList.remove('logo-slider__wrapper--hidden');
+    buttonShowText.textContent = "Cкрыть";
+    let buttonHide = buttonShow;
+    hide(buttonHide);
+})
+}
+function hide (buttonHide) {
+    if (buttonShowText.textContent === "Cкрыть") {
+        buttonHide.addEventListener('click', function(evt){
+            wrapper.classList.add('logo-slider__wrapper--hidden');
+            buttonShowText.textContent = "Показать все";
+            show();
+        })
+    }
+}
+
